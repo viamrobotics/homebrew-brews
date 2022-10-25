584efdd87f9cd57a6dc428d6a3be5b77d3a5b99b
Bijan Haney <bijan@viam.com>
2022-10-25 14:19:27 -0400

    change name of formula

--- Formula/viam-rs-camera-server.rb
+++ Formula/viam-rs-camera-server.rb
@@ -1,14 +1,13 @@
-class ViamRsCameraServer < Formula
+class IntelRealGRPCServer < Formula
   desc "A Viam camera GRPC server for the Intel RealSense"
   homepage "https://www.viam.com/"
   url "https://github.com/viamrobotics/camera-servers/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "928cff021883e0e3740bd8732feb072315afe79bf53d928b8452d5a83ee3a67d"
+  sha256 "4e872fb4ea1710c39c5919a4ed0f6e94d319853cd1ae546abf7eeebc9e87167e"
   license "Apache-2.0"
   head "https://github.com/viamrobotics/camera-servers.git", branch: "main"
 
   depends_on "pkg-config" => :build
   depends_on "go" => :build
-  depends_on "openssl"
   depends_on "grpc"
   depends_on "protobuf"
   depends_on "libhttpserver"
@@ -16,7 +15,6 @@ class ViamRsCameraServer < Formula
   depends_on "librealsense"
 
   def install
-    system "mkdir", "grpc/bin"
     system "make", "buf"
     system "make", "intelrealgrpcserver"
     bin.install "intelrealgrpcserver"
