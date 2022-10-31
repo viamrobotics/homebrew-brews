dc3243745bea122f0def2ba3d2a57e9221941c67
Tess Avitabile <tessavitabile@users.noreply.github.com>
2022-10-31 12:48:48 -0400

    [DATA-605] Orbslam homebrew formula (#4)

---
+++ Formula/orb-grpc-server.rb
@@ -0,0 +1,40 @@
+class OrbGrpcServer < Formula
+  desc "A Viam slam GRPC server for ORB_SLAM3"
+  homepage "https://www.viam.com/"
+  url "https://github.com/viamrobotics/slam.git",
+    tag: "v0.1.5",
+    revision: "392d8bcdc1b122c35d0a5f8c15b2b2a2eb2fd0f3"
+  license "Apache-2.0"
+  head "https://github.com/viamrobotics/slam.git", branch: "main"
+
+  depends_on "pkg-config" => :build
+  depends_on "go" => :build
+  depends_on "cmake" => :build
+  depends_on "grpc"
+  depends_on "glew"
+  depends_on "opencv@4"
+  depends_on "eigen"
+  depends_on "boost"
+  depends_on "openssl"
+  depends_on "pangolin"
+
+  def install
+    chdir "slam-libraries" do
+      system "make", "buf"
+      system "make", "buildorb"
+      if OS.mac?
+        system "install_name_tool", "-change", buildpath.to_s.delete_prefix("/private") + "/slam-libraries/viam-orb-slam3/ORB_SLAM3/Thirdparty/DBoW2/lib/libDBoW2.dylib", "#{lib}/libDBoW2.dylib", "viam-orb-slam3/ORB_SLAM3/lib/libORB_SLAM3.dylib"
+        system "install_name_tool", "-change", buildpath.to_s.delete_prefix("/private") + "/slam-libraries/viam-orb-slam3/ORB_SLAM3/Thirdparty/g2o/lib/libg2o.dylib", "#{lib}/libg2o.dylib", "viam-orb-slam3/ORB_SLAM3/lib/libORB_SLAM3.dylib"
+        system "install_name_tool", "-change", buildpath.to_s.delete_prefix("/private") + "/slam-libraries/viam-orb-slam3/ORB_SLAM3/Thirdparty/DBoW2/lib/libDBoW2.dylib", "#{lib}/libDBoW2.dylib", "viam-orb-slam3/bin/orb_grpc_server"
+        system "install_name_tool", "-change", buildpath.to_s.delete_prefix("/private") + "/slam-libraries/viam-orb-slam3/ORB_SLAM3/Thirdparty/g2o/lib/libg2o.dylib", "#{lib}/libg2o.dylib", "viam-orb-slam3/bin/orb_grpc_server"
+        system "install_name_tool", "-change", buildpath.to_s.delete_prefix("/private") + "/slam-libraries/viam-orb-slam3/ORB_SLAM3/lib/libORB_SLAM3.dylib", "#{lib}/libORB_SLAM3.dylib", "viam-orb-slam3/bin/orb_grpc_server"
+      end
+      bin.install "viam-orb-slam3/bin/orb_grpc_server"
+      lib.install Dir["viam-orb-slam3/ORB_SLAM3/lib/*"]
+      lib.install Dir["viam-orb-slam3/ORB_SLAM3/Thirdparty/DBoW2/lib/*"]
+      lib.install Dir["viam-orb-slam3/ORB_SLAM3/Thirdparty/g2o/lib/*"]
+      (share/"orbslam/Vocabulary").mkpath
+      share.install "viam-orb-slam3/ORB_SLAM3/Vocabulary/ORBvoc.txt" => "orbslam/Vocabulary/ORBvoc.txt"
+    end
+  end
+end
