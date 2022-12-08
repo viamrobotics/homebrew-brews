5e357fcdbe8c4137725559d00f799b149fdff96b
John <john@viam.com>
2022-12-08 15:00:04 -0500

    remove comments

--- Formula/carto-grpc-server.rb
+++ Formula/carto-grpc-server.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class CartoGrpcServer < Formula
   desc "A Viam slam GRPC server for Cartographer"
   homepage "https://www.viam.com/"
