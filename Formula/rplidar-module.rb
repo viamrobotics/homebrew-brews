d3cfbaf486dda39ccff8e4d56d308d89aa5573fd
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2023-01-04 14:18:31 -0500

    Fix rplidar module recipe

--- Formula/rplidar-module.rb
+++ Formula/rplidar-module.rb
@@ -2,8 +2,8 @@ class RplidarModule < Formula
   desc "A Viam rplidar modular component"
   homepage "https://www.viam.com/"
   url "https://github.com/viamrobotics/rplidar.git",
-    tag: "v0.1.0",
-    revision: "50c5527efe1910575cb4b3445871395232a41cb6"
+    tag: "v0.1.1",
+    revision: "4df4e585ac908397b0e8ee881ec79429762ea237"
   license "Apache-2.0"
   head "https://github.com/viamrobotics/rplidar.git", branch: "main"
 
@@ -14,6 +14,6 @@ class RplidarModule < Formula
   def install
     system "make", "swig"
     system "make", "build-module"
-    bin.install "bin/rplidar_module"
+    bin.install "bin/rplidar-module"
   end
 end
