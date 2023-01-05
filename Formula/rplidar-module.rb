b614b5ad4e3eaf676754cbdcd385a2873a824b57
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2023-01-05 15:36:37 -0500

    Fix formulas

--- Formula/rplidar-module.rb
+++ Formula/rplidar-module.rb
@@ -1,18 +1,16 @@
 class RplidarModule < Formula
-  desc "A Viam rplidar modular component"
+  desc "Viam rplidar modular component"
   homepage "https://www.viam.com/"
   url "https://github.com/viamrobotics/rplidar.git",
-    tag: "v0.1.1",
-    revision: "4df4e585ac908397b0e8ee881ec79429762ea237"
+    tag:      "v0.1.3",
+    revision: "64ed987a850f7e15fd451239e6c0d85e9daf6675"
   license "Apache-2.0"
   head "https://github.com/viamrobotics/rplidar.git", branch: "main"
 
   depends_on "go" => :build
   depends_on "swig" => :build
-  depends_on "rplidar-sdk" => :build
 
   def install
-    system "make", "swig"
     system "make", "build-module"
     bin.install "bin/rplidar-module"
   end
