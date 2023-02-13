db3913380091f4b29535c338b145cb86212b0561
Eric Daniels <eric@erdaniels.com>
2023-02-13 15:51:37 -0500

    update intel-real-grpc-server to v0.2.1

--- Formula/intel-real-grpc-server.rb
+++ Formula/intel-real-grpc-server.rb
@@ -1,21 +1,20 @@
 class IntelRealGrpcServer < Formula
-  desc "Viam camera GRPC server for the Intel RealSense"
+  desc "Viam Camera gRPC server for Intel RealSense devices"
   homepage "https://www.viam.com/"
-  url "https://github.com/viamrobotics/camera-servers/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "4e872fb4ea1710c39c5919a4ed0f6e94d319853cd1ae546abf7eeebc9e87167e"
+  url "https://github.com/viamrobotics/camera-servers/archive/refs/tags/v0.2.1.zip"
+  sha256 "e0374b26e0b6a1aecda613287ce07fea28dde63319ad4452bc50f6143ccdae48"
   license "Apache-2.0"
-  head "https://github.com/viamrobotics/camera-servers.git", branch: "main"
+  head "https://github.com/viamrobotics/rdk.git", branch: "main"
 
   depends_on "go" => :build
   depends_on "pkg-config" => :build
   depends_on "grpc"
-  depends_on "libhttpserver"
   depends_on "librealsense"
-  depends_on "opencv"
+  depends_on "jpeg-turbo"
+  depends_on "openssl"
 
   def install
-    system "make", "buf"
-    system "make", "intelrealgrpcserver"
+    system "make", "intelrealgrpcserver-release-opt"
     bin.install "intelrealgrpcserver"
   end
 end
--- README.md
+++ README.md
@@ -9,3 +9,16 @@ Or `brew tap viamrobotics/brews` and then `brew install <formula>`.
 ## Documentation
 
 `brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
+
+## Development
+
+To test a specific formula locally, you can try the following:
+
+`brew reinstall --build-from-source <path-to-homebrew-brews>/Formula/<formula>.rb`
+
+To test uncommitted code that the formula relies on:
+1. Temporarily, until ready to commit the formula
+	1. Remove the sha256, head field
+	2. Change url to be a tarball you create locally of your code (e.g. `tar -C <path-to-repo> -czf some.tgz .`) using `url "file:///<some-absolute-path-to>/some.tgz"`. You'll want to make sure the directory is cleaned of build artifacts before hand.
+	3. Set version to some bogus value
+2. When done, undo these changes and use the normal tooling to bump the version.
