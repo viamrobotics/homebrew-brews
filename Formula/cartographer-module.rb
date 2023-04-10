54c264f53ced0235f98cc3f7ec454ae54c07f553
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2023-04-10 13:52:48 -0400

    Add cartographer module brew package

---
+++ Formula/cartographer-module.rb
@@ -0,0 +1,49 @@
+class CartographerModule < Formula
+  desc "Viam cartographer modular service"
+  homepage "https://www.viam.com/"
+  url "https://github.com/viamrobotics/viam-cartographer.git",
+    tag:      "v0.3.1",
+    revision: "5c3e8df6003d90944f8b45773fef5e5158a3ad0f"
+  license "Apache-2.0"
+  head "https://github.com/viamrobotics/viam-cartographer.git", branch: "main"
+
+  depends_on "cmake" => :build
+  depends_on "go" => :build
+  depends_on "ninja" => :build
+  depends_on "abseil"
+  depends_on "boost"
+  depends_on "cairo"
+  depends_on "ceres-solver"
+  depends_on "eigen"
+  depends_on "gflags"
+  depends_on "glog"
+  depends_on "googletest"
+  depends_on "grpc"
+  depends_on "lua@5.3"
+  depends_on "openssl"
+  depends_on "pcl"
+  depends_on "protobuf"
+  depends_on "sphinx-doc"
+  depends_on "suite-sparse"
+
+  def install
+    system "make", "buf"
+    system "make", "build"
+
+    bin.install "bin/cartographer-module"
+    bin.install "viam-cartographer/build/carto_grpc_server"
+    lib.install "viam-cartographer/cartographer/build/libcartographer.a"
+    lib.install "viam-cartographer/build/libviam-cartographer.a"
+    (share/"cartographer/lua_files").mkpath
+    share.install "viam-cartographer/lua_files/locating_in_map.lua" => "cartographer/lua_files/"
+    share.install "viam-cartographer/lua_files/mapping_new_map.lua" => "cartographer/lua_files/"
+    share.install "viam-cartographer/lua_files/updating_a_map.lua" => "cartographer/lua_files/"
+    share.install "viam-cartographer/cartographer/configuration_files/map_builder.lua" => "cartographer/lua_files/"
+    share.install "viam-cartographer/cartographer/configuration_files/pose_graph.lua" => "cartographer/lua_files/"
+    share.install "viam-cartographer/cartographer/configuration_files/trajectory_builder_2d.lua" => "cartographer/lua_files/"
+    share.install "viam-cartographer/cartographer/configuration_files/map_builder_server.lua" => "cartographer/lua_files/"
+    share.install "viam-cartographer/cartographer/configuration_files/trajectory_builder.lua" => "cartographer/lua_files/"
+    share.install "viam-cartographer/cartographer/configuration_files/trajectory_builder_3d.lua" => "cartographer/lua_files/"
+
+  end
+end
