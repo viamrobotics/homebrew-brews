2e9583493227d21128e03ac23224e2e141259a81
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2023-04-12 13:52:26 -0400

    RSDK-2022 - Add the cartographer module brew package (#21)

--- Formula/cartographer-module.rb
+++ Formula/cartographer-module.rb
@@ -1,5 +1,5 @@
-class CartoGrpcServer < Formula
-  desc "Viam slam GRPC server for Cartographer"
+class CartographerModule < Formula
+  desc "Viam cartographer modular service"
   homepage "https://www.viam.com/"
   url "https://github.com/viamrobotics/viam-cartographer.git",
     tag:      "v0.3.1",
@@ -7,34 +7,21 @@ class CartoGrpcServer < Formula
   license "Apache-2.0"
   head "https://github.com/viamrobotics/viam-cartographer.git", branch: "main"
 
-  conflicts_with "cartographer-module", because: "cartographer-module also installs carto_grpc_server"
+  conflicts_with "carto-grpc-server", because: "carto-grpc-server also installs carto_grpc_server"
 
   depends_on "cmake" => :build
   depends_on "go" => :build
   depends_on "ninja" => :build
-  depends_on "abseil"
-  depends_on "boost"
-  depends_on "cairo"
-  depends_on "ceres-solver"
-  depends_on "eigen"
-  depends_on "gflags"
-  depends_on "glog"
-  depends_on "googletest"
-  depends_on "grpc"
-  depends_on "lua@5.3"
-  depends_on "openssl"
-  depends_on "pcl"
   depends_on "protobuf"
-  depends_on "sphinx-doc"
-  depends_on "suite-sparse"
+  depends_on "grpc"
+  depends_on "pcl"
 
   def install
     system "make", "buf"
     system "make", "build"
 
+    bin.install "bin/cartographer-module"
     bin.install "viam-cartographer/build/carto_grpc_server"
-    lib.install "viam-cartographer/cartographer/build/libcartographer.a"
-    lib.install "viam-cartographer/build/libviam-cartographer.a"
     (share/"cartographer/lua_files").mkpath
     share.install "viam-cartographer/lua_files/locating_in_map.lua" => "cartographer/lua_files/"
     share.install "viam-cartographer/lua_files/mapping_new_map.lua" => "cartographer/lua_files/"
@@ -45,5 +32,6 @@ class CartoGrpcServer < Formula
     share.install "viam-cartographer/cartographer/configuration_files/map_builder_server.lua" => "cartographer/lua_files/"
     share.install "viam-cartographer/cartographer/configuration_files/trajectory_builder.lua" => "cartographer/lua_files/"
     share.install "viam-cartographer/cartographer/configuration_files/trajectory_builder_3d.lua" => "cartographer/lua_files/"
+
   end
 end
