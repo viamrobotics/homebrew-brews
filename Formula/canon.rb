05b320bc36e7bee0e3b02709f2a5faa65f878e3d
Otterverse <Otterverse@users.noreply.github.com>
2023-07-28 19:05:53 +0000

    Auto-update of package versions

--- Formula/canon.rb
+++ Formula/canon.rb
@@ -1,8 +1,8 @@
 class Canon < Formula
   desc "CLI utility for managing docker-based, canonical development environments"
   homepage "https://github.com/viamrobotics/canon"
-  url "https://github.com/viamrobotics/canon/archive/refs/tags/v0.2.2.tar.gz"
-  sha256 "bb334efc3b2647b47f5d370cf14634a90f49c6f97d7d979424deb26c1bf1b477"
+  url "https://github.com/viamrobotics/canon/archive/refs/tags/v0.2.3.tar.gz"
+  sha256 "16a3c7626d2b34bdbfe24aa0b49c35cdbc3c1ba8efbdf6958c61fd99fc7f3a1c"
   head "https://github.com/viamrobotics/canon.git", branch: "main"
 
   depends_on "go" => :build
