161629fca2204c4b76eb462481bae22f9bcc243c
Benjamin Rewis <32186188+benjirewis@users.noreply.github.com>
2023-08-01 11:39:41 -0700

    RSDK-3446 Add formula for dialdbg (#32)

---
+++ Formula/viam-dialdbg.rb
@@ -0,0 +1,16 @@
+class ViamDialdbg < Formula
+  desc "Viam dial debugger"
+  homepage "https://www.viam.com/"
+  url "https://github.com/viamrobotics/rust-utils.git",
+    tag:      "v0.0.26",
+    revision: "056b0de4c817d3ab84440bfee275ec173126149c"
+  license "Apache-2.0"
+  head "https://github.com/viamrobotics/rust-utils.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "make", "build-dialdbg"
+    bin.install "target/release/viam-dialdbg"
+  end
+end
