227232d49bc9396d5afdefbfa3fc1e7895cda1bd
Otterverse <Otterverse@users.noreply.github.com>
2023-08-17 17:06:19 +0000

    Auto-update of package versions

--- Formula/viam-dialdbg.rb
+++ Formula/viam-dialdbg.rb
@@ -2,8 +2,8 @@ class ViamDialdbg < Formula
   desc "Viam dial debugger"
   homepage "https://www.viam.com/"
   url "https://github.com/viamrobotics/rust-utils.git",
-    tag:      "v0.0.26",
-    revision: "056b0de4c817d3ab84440bfee275ec173126149c"
+    tag:      "v0.0.27",
+    revision: "f1315bc87535e32e401e4183d9a0a535e1af7a2f"
   license "Apache-2.0"
   head "https://github.com/viamrobotics/rust-utils.git", branch: "main"
 
