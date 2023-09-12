bede47061bef36deab3175fd5f059b446e9f0156
Abe Winter <abe.winter@viam.com>
2023-09-12 11:56:58 -0400

    add head to support livecheck in bump, add sha to suppress warning

--- Formula/viam-cli.rb
+++ Formula/viam-cli.rb
@@ -1,6 +1,7 @@
 class ViamCli < Formula
-  version = "v0.8.0"
-  url "https://github.com/viamrobotics/rdk/archive/refs/tags/#{version}.tar.gz"
+  url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.8.0.tar.gz"
+  sha256 "ec8f0f5a4dda80de5e36e3241a219c0d7e98225754ae0c994dbcd2e4b2d32dd5"
+  head "https://github.com/viamrobotics/rdk.git", branch: "main"
 
   depends_on "go" => :build
 
