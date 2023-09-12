fffebf844fa9c24958bfd864816c5570d575cc03
Abe Winter <abe.winter@viam.com>
2023-09-12 17:53:02 -0400

    rename viam-cli -> viam to match command name, add description

--- Formula/viam.rb
+++ Formula/viam.rb
@@ -1,7 +1,8 @@
-class ViamCli < Formula
+class Viam < Formula
   url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.8.0.tar.gz"
   sha256 "ec8f0f5a4dda80de5e36e3241a219c0d7e98225754ae0c994dbcd2e4b2d32dd5"
   head "https://github.com/viamrobotics/rdk.git", branch: "main"
+  desc "Viam CLI for managing your modules and auth keys. (See also: viam-server for running a robot)."
 
   depends_on "go" => :build
 
