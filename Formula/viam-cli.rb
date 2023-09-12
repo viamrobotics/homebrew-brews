82957efa878f399c15d75057f69248be59a440f8
Abe Winter <abe.winter@viam.com>
2023-09-12 11:38:51 -0400

    source install working on mac

--- Formula/viam-cli.rb
+++ Formula/viam-cli.rb
@@ -1,17 +1,12 @@
 class ViamCli < Formula
-  arch = Hardware::CPU.intel? ? "amd64" : "arm64"
-  version = "0.8.0"
-  os_name = OS.linux? ? "linux" : "darwin"
-  platform = "#{os_name}-#{arch}"
-  desc "CLI for admin tasks in your Viam account"
-  homepage "https://docs.viam.com/manage/cli/"
-  shas = {
-    "linux-amd64" => "5d4759f18ed4772dadf72aa318b503bfb7e2f963ed9553ce7d4cea45c0caed93"
-  }
-  sha256 shas[platform]
-  url "https://storage.googleapis.com/packages.viam.com/apps/viam-cli/viam-cli-v#{version}-#{platform}"
+  version = "v0.8.0"
+  url "https://github.com/viamrobotics/rdk/archive/refs/tags/#{version}.tar.gz"
+
+  depends_on "go" => :build
 
   def install
-    bin.install "packages.viam.com" => "viam"
+    ENV["TAG_VERSION"] = version
+    system("make", "cli-ci")
+    bin.install Dir["bin/*/viam-cli"][0] => "viam"
   end
 end
