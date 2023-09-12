e10bd6cebe0397ada2ceeb7d50252ec5c2693523
abe-winter <abe-winter@users.noreply.github.com>
2023-09-12 18:33:48 -0400

    change formula desc
    Co-authored-by: James Otting <james@viam.com>

--- Formula/viam.rb
+++ Formula/viam.rb
@@ -2,7 +2,7 @@ class Viam < Formula
   url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.8.0.tar.gz"
   sha256 "ec8f0f5a4dda80de5e36e3241a219c0d7e98225754ae0c994dbcd2e4b2d32dd5"
   head "https://github.com/viamrobotics/rdk.git", branch: "main"
-  desc "Viam CLI for managing your modules and auth keys. (See also: viam-server for running a robot)."
+  desc "Viam CLI for managing robots, modules, orgs, etc. on the Viam platform. (See also: viam-server for running a robot)."
 
   depends_on "go" => :build
 
