cf7b2e3b000a71c0e22eed97b1a6ff7ebb234023
Abe Winter <abe-winter@users.noreply.github.com>
2023-09-15 16:58:15 -0400

    pin to commit and label 0.8.1a

--- Formula/viam.rb
+++ Formula/viam.rb
@@ -1,13 +1,13 @@
 class Viam < Formula
-  url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.8.1.tar.gz"
-  sha256 "7d07ddbf51e5fbe855c1b3d5564ef7a1d8fc18519f7917e6f889e11cf8a37206"
+  url "https://github.com/viamrobotics/rdk/archive/f4fe50894438be446d1734a6c86787d36da1e632.tar.gz"
+  sha256 "fbed9f6e2b1bddf1ff435a12d142e07cf6ce55d6f20d73103c217c171daf2599"
   head "https://github.com/viamrobotics/rdk.git", branch: "main"
   desc "Viam CLI for managing robots, modules, orgs, etc. on the Viam platform. (See also: viam-server for running a robot)."
 
   depends_on "go" => :build
 
   def install
-    ENV["TAG_VERSION"] = version
+    ENV["TAG_VERSION"] = "0.8.1a"
     system("make", "cli-ci")
     bin.install Dir["bin/*/viam-cli"][0] => "viam"
   end
