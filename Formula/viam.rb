2c856b90038597d1778b0acbab4b1081d74252b2
Abe Winter <abe-winter@users.noreply.github.com>
2023-09-15 17:28:54 -0400

    specify version in outer scope so brew knows to upgrade

--- Formula/viam.rb
+++ Formula/viam.rb
@@ -3,11 +3,12 @@ class Viam < Formula
   sha256 "fbed9f6e2b1bddf1ff435a12d142e07cf6ce55d6f20d73103c217c171daf2599"
   head "https://github.com/viamrobotics/rdk.git", branch: "main"
   desc "Viam CLI for managing robots, modules, orgs, etc. on the Viam platform. (See also: viam-server for running a robot)."
+  version "0.8.1a"
 
   depends_on "go" => :build
 
   def install
-    ENV["TAG_VERSION"] = "0.8.1a"
+    ENV["TAG_VERSION"] = version
     system("make", "cli-ci")
     bin.install Dir["bin/*/viam-cli"][0] => "viam"
   end
