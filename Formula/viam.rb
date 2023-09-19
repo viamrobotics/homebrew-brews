3659c294c8730167e2f3bf3fc4d8c339e0e63db3
Abe Winter <abe-winter@users.noreply.github.com>
2023-09-19 12:41:50 -0400

    Revert "hotfix viam.rb 0.8.1a (#34)"
    This reverts commit 7825a2a7cc9817dc8f94fa921d0421c0b0502a05.

--- Formula/viam.rb
+++ Formula/viam.rb
@@ -1,9 +1,8 @@
 class Viam < Formula
-  url "https://github.com/viamrobotics/rdk/archive/f4fe50894438be446d1734a6c86787d36da1e632.tar.gz"
-  sha256 "fbed9f6e2b1bddf1ff435a12d142e07cf6ce55d6f20d73103c217c171daf2599"
+  url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.8.1.tar.gz"
+  sha256 "7d07ddbf51e5fbe855c1b3d5564ef7a1d8fc18519f7917e6f889e11cf8a37206"
   head "https://github.com/viamrobotics/rdk.git", branch: "main"
   desc "Viam CLI for managing robots, modules, orgs, etc. on the Viam platform. (See also: viam-server for running a robot)."
-  version "0.8.1a"
 
   depends_on "go" => :build
 
