11c3478f5fb56815da613fc4cae0d115fe75f1c7
Otterverse <Otterverse@users.noreply.github.com>
2023-09-19 20:06:21 +0000

    Auto-update of package versions

--- Formula/cartographer-module.rb
+++ Formula/cartographer-module.rb
@@ -2,8 +2,8 @@ class CartographerModule < Formula
   desc "Viam cartographer modular service"
   homepage "https://www.viam.com/"
   url "https://github.com/viamrobotics/viam-cartographer.git",
-    tag:      "v0.3.28",
-    revision: "2c49a07e81a48faf8e3ed198adaf47da5740e57e"
+    tag:      "v0.3.29",
+    revision: "d73addf1b066103aaacd75a87c5fae8902901f4c"
   license "Apache-2.0"
   head "https://github.com/viamrobotics/viam-cartographer.git", branch: "main"
 
