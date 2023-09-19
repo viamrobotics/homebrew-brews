48a0ebec31a559a5c51ab8e432071be7ba4e1df7
Abe Winter <abe-winter@users.noreply.github.com>
2023-09-19 12:43:55 -0400

    update viam.rb to stable

--- Formula/viam.rb
+++ Formula/viam.rb
@@ -1,6 +1,6 @@
 class Viam < Formula
-  url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.8.1.tar.gz"
-  sha256 "7d07ddbf51e5fbe855c1b3d5564ef7a1d8fc18519f7917e6f889e11cf8a37206"
+  url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.9.1.tar.gz"
+  sha256 "fcc1e791636e10153f84dff10c8c94c72e2f7358f86cf9fa3f8be59d23e80ba5"
   head "https://github.com/viamrobotics/rdk.git", branch: "main"
   desc "Viam CLI for managing robots, modules, orgs, etc. on the Viam platform. (See also: viam-server for running a robot)."
 
