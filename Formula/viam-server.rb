331ee40ac65e4f37845ba496130caef6c6176c45
Otterverse <Otterverse@users.noreply.github.com>
2023-11-29 17:06:14 +0000

    Auto-update of package versions

--- Formula/viam-server.rb
+++ Formula/viam-server.rb
@@ -1,8 +1,8 @@
 class ViamServer < Formula
   desc "Main server application of the viam robot development kit (RDK)"
   homepage "https://www.viam.com/"
-  url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.14.0.tar.gz"
-  sha256 "25eef4148b94b856daa46c125d67d9c2cbdd6d393ae0092c72d164a418b8dc8e"
+  url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.15.0.tar.gz"
+  sha256 "5ca465f472dba320e7b57f5e42cc4c373ebfdac5deaa70524569cfb89ab2d5fc"
   license "AGPL-3.0"
   head "https://github.com/viamrobotics/rdk.git", branch: "main"
 
