315d7ad7d592549c477d6d407e53769a61194804
Otterverse <Otterverse@users.noreply.github.com>
2023-12-05 22:05:51 +0000

    Auto-update of package versions

--- Formula/rplidar-module.rb
+++ Formula/rplidar-module.rb
@@ -2,8 +2,8 @@ class RplidarModule < Formula
   desc "Viam rplidar modular component"
   homepage "https://www.viam.com/"
   url "https://github.com/viamrobotics/rplidar.git",
-    tag:      "v0.1.15",
-    revision: "034bc404e3cca72ff378d7b8da3d3365733ef1b7"
+    tag:      "v0.1.16",
+    revision: "1a698aa9f99f5d45f2f43443982af1138d586010"
   license "Apache-2.0"
   head "https://github.com/viamrobotics/rplidar.git", branch: "main"
 
