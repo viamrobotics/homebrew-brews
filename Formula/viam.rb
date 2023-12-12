b8ae64264b802e72aecff57d9d3235afb5c3a74a
Zack Porter <121693134+zaporter-work@users.noreply.github.com>
2023-12-12 12:43:57 -0500

    Revert "Out of band CLI release (#38)"
    This reverts commit c9518129cb10908db90f5a7660136c60074796c6.

--- Formula/viam.rb
+++ Formula/viam.rb
@@ -1,9 +1,8 @@
 class Viam < Formula
-  url "https://github.com/viamrobotics/rdk/archive/9781684852fd8645c7f3861ac1f1ddfcaeb0d0c9.tar.gz"
-  sha256 "ed05e24e528f8ae56a11563d7fd841d6ed35414f7a4eb7b120a144c62ad1e17e"
+  url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.15.0.tar.gz"
+  sha256 "5ca465f472dba320e7b57f5e42cc4c373ebfdac5deaa70524569cfb89ab2d5fc"
   head "https://github.com/viamrobotics/rdk.git", branch: "main"
   desc "Viam CLI for managing robots, modules, orgs, etc. on the Viam platform. (See also: viam-server for running a robot)."
-  version "0.15.0a"
 
   depends_on "go" => :build
 
