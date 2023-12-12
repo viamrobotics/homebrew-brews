b80b3bf812fdfe2889ed5f90b0fefdc180fa42d8
zaporter-work <work-github@zackporter.com>
2023-12-12 12:48:48 -0500

    to 16

--- Formula/viam.rb
+++ Formula/viam.rb
@@ -1,6 +1,6 @@
 class Viam < Formula
-  url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.15.0.tar.gz"
-  sha256 "5ca465f472dba320e7b57f5e42cc4c373ebfdac5deaa70524569cfb89ab2d5fc"
+  url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.16.0.tar.gz"
+  sha256 "562ff529fa5e7c7b3f84687dc91755beb14d49c7977a3cd438147bae9c5da96a"
   head "https://github.com/viamrobotics/rdk.git", branch: "main"
   desc "Viam CLI for managing robots, modules, orgs, etc. on the Viam platform. (See also: viam-server for running a robot)."
 
