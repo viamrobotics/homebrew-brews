c852334a243a7c0a234f175e7d6267639259bcfb
Zack Porter <121693134+zaporter-work@users.noreply.github.com>
2023-12-12 13:56:46 -0500

    Revert zp/out of band fix (#42)

--- Formula/viam.rb
+++ Formula/viam.rb
@@ -1,9 +1,8 @@
 class Viam < Formula
-  url "https://github.com/viamrobotics/rdk/archive/9781684852fd8645c7f3861ac1f1ddfcaeb0d0c9.tar.gz"
-  sha256 "ed05e24e528f8ae56a11563d7fd841d6ed35414f7a4eb7b120a144c62ad1e17e"
+  url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.16.0.tar.gz"
+  sha256 "562ff529fa5e7c7b3f84687dc91755beb14d49c7977a3cd438147bae9c5da96a"
   head "https://github.com/viamrobotics/rdk.git", branch: "main"
   desc "Viam CLI for managing robots, modules, orgs, etc. on the Viam platform. (See also: viam-server for running a robot)."
-  version "0.15.0a"
 
   depends_on "go" => :build
 
