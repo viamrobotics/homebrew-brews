1587cda83fd5d3c3b23cae9518e69ba9d2990e74
Ethan <ethanrodkin@proton.me>
2024-02-13 14:10:23 -0500

    RSDK-3055 - add viam-cpp-sdk to brew (#44)

---
+++ Formula/viam-cpp-sdk.rb
@@ -0,0 +1,26 @@
+class ViamCppSdk < Formula
+  desc "Viam C++ SDK"
+  homepage "https://www.viam.com/"
+  url "https://github.com/viamrobotics/viam-cpp-sdk.git",
+    tag:      "releases/v0.0.1",
+    revision: "faf5d769badf5a3c48a112511fdd122fcfc573b6"
+  license "Apache-2.0"
+  head "https://github.com/viamrobotics/viam-cpp-sdk.git", branch: "main"
+
+  depends_on "cmake" => :build
+  depends_on "gcc" => :build
+  depends_on "abseil"
+  depends_on "boost"
+  depends_on "grpc"
+  depends_on "protobuf"
+  depends_on "xtensor"
+  depends_on "pkg-config"
+  depends_on "ninja"
+  depends_on "buf"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+end
