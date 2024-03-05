d24c007e5751ab7a8d6e29123f72a1f49ed8aa3a
Otterverse <Otterverse@users.noreply.github.com>
2024-03-05 17:06:23 +0000

    Auto-update of package versions

--- Formula/viam-server.rb
+++ Formula/viam-server.rb
@@ -1,8 +1,8 @@
 class ViamServer < Formula
   desc "Main server application of the viam robot development kit (RDK)"
   homepage "https://www.viam.com/"
-  url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.21.2.tar.gz"
-  sha256 "87863dbbf0b7c7b13c20f8d23433abf9e02b5961b5a44041bc3bd3875a1d4cb2"
+  url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.22.0.tar.gz"
+  sha256 "00f17faea07b5a3c7b49b3be56d835cb6147a37c7ff6c9f6b0d5ea2c3c00f54c"
   license "AGPL-3.0"
   head "https://github.com/viamrobotics/rdk.git", branch: "main"
 
--- Formula/viam.rb
+++ Formula/viam.rb
@@ -1,8 +1,8 @@
 class Viam < Formula
   desc "CLI for managing robots, orgs, etc. (See viam-server for running a robot)"
   homepage "https://docs.viam.com/fleet/cli/"
-  url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.21.2.tar.gz"
-  sha256 "87863dbbf0b7c7b13c20f8d23433abf9e02b5961b5a44041bc3bd3875a1d4cb2"
+  url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.22.0.tar.gz"
+  sha256 "00f17faea07b5a3c7b49b3be56d835cb6147a37c7ff6c9f6b0d5ea2c3c00f54c"
   head "https://github.com/viamrobotics/rdk.git", branch: "main"
 
   depends_on "go" => :build
