72784b2820bb27d45304177d2f3cd3fedbec63d0
Otterverse <Otterverse@users.noreply.github.com>
2024-05-14 14:06:37 +0000

    Auto-update of package versions

--- Formula/viam.rb
+++ Formula/viam.rb
@@ -1,8 +1,8 @@
 class Viam < Formula
   desc "CLI for managing robots, orgs, etc. (See viam-server for running a robot)"
   homepage "https://docs.viam.com/fleet/cli/"
-  url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.26.0.tar.gz"
-  sha256 "fb1582f9bb98b361b763d3f3066f4c9ec2b45c407d26de4a02b4a80ef61e9347"
+  url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.27.0.tar.gz"
+  sha256 "3c5abf0327a4ef4c64842bd1be27d4bef4356202bfc148b15541938f66cd264d"
   head "https://github.com/viamrobotics/rdk.git", branch: "main"
 
   depends_on "go" => :build
