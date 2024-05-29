6f0dccd38057c113ce0918bf5340d96f6be29df2
Zack Porter <121693134+zaporter-work@users.noreply.github.com>
2024-05-29 13:16:51 -0400

    Viam cli docs link out of date (#46)

--- Formula/viam.rb
+++ Formula/viam.rb
@@ -1,6 +1,6 @@
 class Viam < Formula
   desc "CLI for managing robots, orgs, etc. (See viam-server for running a robot)"
-  homepage "https://docs.viam.com/fleet/cli/"
+  homepage "https://docs.viam.com/cli/"
   url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.28.0.tar.gz"
   sha256 "17613301a6dc9e5117452424dcb1b203b228942edb58c80726f61c41d5e6ef54"
   head "https://github.com/viamrobotics/rdk.git", branch: "main"
