4bb04f5b9b28f407d48ae5a2dbb1471da23b759f
Otterverse <Otterverse@users.noreply.github.com>
2024-11-04 20:07:21 +0000

    Auto-update of package versions

--- Formula/viam-cpp-sdk.rb
+++ Formula/viam-cpp-sdk.rb
@@ -2,8 +2,8 @@ class ViamCppSdk < Formula
   desc "Viam C++ SDK"
   homepage "https://www.viam.com/"
   url "https://github.com/viamrobotics/viam-cpp-sdk.git",
-    tag:      "releases/v0.0.13",
-    revision: "d5000dd948a043a75dcb2a706afee881a1f56de0"
+    tag:      "releases/v0.0.14",
+    revision: "69e17588e8e1b3349e1079f66dc54fd1b72badb6"
   license "Apache-2.0"
   head "https://github.com/viamrobotics/viam-cpp-sdk.git", branch: "main"
 
