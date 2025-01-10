a49e8e8dfdce8b27542fb5486eea84bcee02923f
Max Horowitz <maxhorowitz@gmail.com>
2025-01-10 17:36:18 -0500

    Fix issue with git history.

--- Formula/viam.rb
+++ Formula/viam.rb
@@ -7,7 +7,7 @@ class Viam < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/viamrobotics/brews"
-    rebuild 15
+    rebuild 14
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "2cc09b897d9f6c3c750617fa737527bf325a13d522a1bb16382781f4cc638806"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:  "17b052c3d91e87dae0bfe6b99e96d07590f140fab4507971b7cdf30a1e9938a2"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "a30065094512311114af00b5a13d6d1d0df1300cd9a9c26be4ffdee34e68b904"
