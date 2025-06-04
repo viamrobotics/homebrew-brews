4345424af88a210edea62f46ca7263a4dc53ed1a
abe-winter <abe-winter@users.noreply.github.com>
2025-06-04 23:04:37 +0000

    Auto-update of package versions

--- Formula/viam-server.rb
+++ Formula/viam-server.rb
@@ -1,8 +1,8 @@
 class ViamServer < Formula
   desc "Main server application of the viam robot development kit (RDK)"
   homepage "https://www.viam.com/"
-  url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.78.0.tar.gz"
-  sha256 "8f5af4f5cf09869c09413dfd2af598008da74ff516acbf18b963f21d4cc9ff3d"
+  url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.78.1.tar.gz"
+  sha256 "4934100950e06fdf6b660ab8b6f2543e7e73a02e0be3bfed5ff8302a11b54c12"
   license "AGPL-3.0"
   head "https://github.com/viamrobotics/rdk.git", branch: "main"
 
