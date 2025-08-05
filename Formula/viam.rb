3ea4fc44a53b441fe45d28d24ac522c791dd721d
abe-winter <abe-winter@users.noreply.github.com>
2025-08-05 19:07:10 +0000

    Auto-update of package versions

--- Formula/viam.rb
+++ Formula/viam.rb
@@ -1,8 +1,8 @@
 class Viam < Formula
   desc "CLI for managing robots, orgs, etc. (See viam-server for running a robot)"
   homepage "https://docs.viam.com/cli/"
-  url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.86.2.tar.gz"
-  sha256 "4d98690aa9e2a87ee17003b81d0ddbfe22af5f86a6792d760e378e743c89a025"
+  url "https://github.com/viamrobotics/rdk/archive/refs/tags/v0.87.0.tar.gz"
+  sha256 "46b4765eeef92474cf1dc2488da89ba98629b96e41a0133808d8a72c9abab5c7"
   head "https://github.com/viamrobotics/rdk.git", branch: "main"
 
   bottle do
