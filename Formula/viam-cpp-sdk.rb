b19ca2ab46c5438d870081d8d0eb14b1b9e65142
abe-winter <abe-winter@users.noreply.github.com>
2025-09-02 20:04:46 +0000

    Auto-update of package versions

--- Formula/viam-cpp-sdk.rb
+++ Formula/viam-cpp-sdk.rb
@@ -2,8 +2,8 @@ class ViamCppSdk < Formula
   desc "Viam C++ SDK"
   homepage "https://www.viam.com/"
   url "https://github.com/viamrobotics/viam-cpp-sdk.git",
-    tag:      "releases/v0.18.0",
-    revision: "3829c8f42778c1f0e2f531fe790a0d6c53f3111e"
+    tag:      "releases/v0.19.0",
+    revision: "1ed59dfe43ff1ecaa3899a85f15090a59eb83473"
   license "Apache-2.0"
   head "https://github.com/viamrobotics/viam-cpp-sdk.git", branch: "main"
 
