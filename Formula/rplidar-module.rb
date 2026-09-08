f59610daaf3439a3e64342c557f483f43fae140c
abe-winter <abe-winter@users.noreply.github.com>
2026-09-08 21:07:19 +0000

    Auto-update of package versions

--- Formula/rplidar-module.rb
+++ Formula/rplidar-module.rb
@@ -2,8 +2,8 @@ class RplidarModule < Formula
   desc "Viam rplidar modular component"
   homepage "https://www.viam.com/"
   url "https://github.com/viamrobotics/rplidar.git",
-    tag:      "v0.1.20",
-    revision: "58eb42040bc22c55f7dc99984f3f6229102913fc"
+    tag:      "v0.1.21",
+    revision: "1c7ed041fbb98416c47d9a9b7cbb992c52e62e81"
   license "Apache-2.0"
   head "https://github.com/viamrobotics/rplidar.git", branch: "main"
 
